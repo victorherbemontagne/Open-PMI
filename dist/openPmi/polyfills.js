75d6cdfe09e9311f9b6b7876c1d1a4516f235fd5
Victor D'herbemont <victordherbemont@gmail.com>
2022-09-17 13:41:40 +0200

    NEW DATA

--- dist/openPmi/polyfills.js
+++ dist/openPmi/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\DELL\Desktop\Gazouyi\Extra\Open-PMI\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! C:\Users\Microsoft\Desktop\Gazouyi\Open-PMI\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
